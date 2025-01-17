764e7748fa74b944f3a058eb2c32a7649be8e7f7
DaveL1011 <davelindians11@gmail.com>
2025-01-17 11:56:20 -0500

    Hello World

---
+++ main_app/src/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HomePage extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default HomePage;
